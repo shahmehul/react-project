0e3d001743f3755f0fe3bfbf1c50874df194af99
shahmehul <shahmehul20@gmail.com>
2025-11-05 10:52:57 -0800

    kanban board.

--- src/Components/Kanban/Kanban.tsx
+++ src/Components/Kanban/Kanban.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import './Kanban.css';
 
+type status = "todo" | "inProgress" | "done";
+
 function Kanban(){
     const [tasks, setTasks] = useState({
         todo: { id: 1, values: ["vacuum", "wash dishes"], label: 'To Do'},
@@ -8,49 +10,51 @@ function Kanban(){
         done: { id: 3, values: ["mow lawn"], label: 'Done'},
     });
 
+    const [errorMessage, setErrorMessage] = useState<string>('')
+
+    const stages:status[] = ['todo','inProgress','done'];
+
+    const onTaskClick = (currentTask:any, currentStage: status)=> {
+        setErrorMessage('');
+        const indexOfCurrentStage = stages.indexOf(currentStage);
+        const nextStage = stages[indexOfCurrentStage+1];
+        if(!nextStage){
+            setErrorMessage('this task is already complete');
+            return;
+        }
+
+        setTasks( prev => ({
+            ...prev,
+            [currentStage]: {
+                ...prev[currentStage],
+                values: prev[currentStage].values.filter((t: any) => t !== currentTask)
+            },
+            [nextStage]: {
+                ...prev[nextStage],
+                values: [...prev[nextStage].values, currentTask]
+            }
+        }))
+    }
+
     return (
         <>
             <h1> Kanban Board</h1>
+            <p className="error-message"> {errorMessage} </p>
             <div className='board-container'>
-                <div>
-                    
-                    <div className="items-container">
-                    <h3> To Do </h3>
-                        {        
-                            tasks.todo.values.map((task) => (
-                                <div key={tasks.todo.id} className="item">
-                                    <p>{task}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-                <div>
-                    
-                    <div className="items-container">
-                    <h3> In Progress </h3>
-                        {
-                            tasks.inProgress.values.map((task) => (
-                                <div key={tasks.inProgress.id} className="item">
-                                    <p>{task}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-                <div>
-                    
-                    <div className="items-container">
-                    <h3> Done </h3>
-                        {
-                            tasks.done.values.map((task) => (
-                                <div key={tasks.done.id} className="item">
-                                    <p>{task}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
+                {
+                    Object.entries(tasks).map(([columnKey, columnValue]) => (
+                        <div className="items-container">
+                            <h3> {columnValue.label} </h3>
+                            {
+                                columnValue.values?.map((task, index) => (
+                                    <div key={index} className="item">
+                                        <p onClick={() => onTaskClick(task, columnKey as status)}>{task}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
